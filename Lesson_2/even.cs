75e4c6c2b9c5473973131947911c9540920f1c33
hurma <karaulnihiv@gmail.com>
2021-02-17 00:43:45 +0300

    complete 3 task

---
+++ Lesson_2/even.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace even
+{
+    class even
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Четное");
+            }
+            else
+            {
+                Console.WriteLine("Нечетное");
+            }
+        }
+    }
+}
