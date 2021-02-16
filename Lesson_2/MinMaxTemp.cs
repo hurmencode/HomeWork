b3da878e5488e53e65649cdc5c644ea9591d84c9
hurma <karaulnihiv@gmail.com>
2021-02-17 00:15:13 +0300

    complete 1 task

---
+++ Lesson_2/MinMaxTemp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lesson_2
+{
+    class MinMaxTemp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите минимальную и максимальную температуру");
+            double min = Convert.ToInt32(Console.ReadLine());
+            double max = Convert.ToInt32(Console.ReadLine());
+            double ave = (min + max) / 2;
+            Console.WriteLine($"min= {min}; max= {max}; ave = {ave}");
+        }
+    }
+}
