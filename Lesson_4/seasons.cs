17b5c0b48492ae39a9626d4b3713df22e223ff97
hurma <karaulnihiv@gmail.com>
2021-03-01 01:39:30 +0300

    completing 3 task

---
+++ Lesson_4/seasons.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace seasons
+{
+    class seasons
+    {
+        enum Season
+        {
+            Winter,
+            Spring,
+            Summer,
+            Autumn
+        }
+        static void Main(string[] args)
+        {
+            int num = Seasons();
+            if (num > 12)
+            {
+                Console.WriteLine("Ошибка: введите число от 1 до 12");
+            }
+
+        }
+
+        static int Seasons()
+        {
+            Console.WriteLine("Введите номер месяца");
+            int numberMonth =Convert.ToInt32(Console.ReadLine());
+            switch (numberMonth)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine(Season.Winter);
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine(Season.Spring);
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine(Season.Summer);
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine(Season.Autumn);
+                    break;
+            }
+            return numberMonth;
+        }
+    }
+}
