4c39b5e9e639d04664b39416288e54ed68fcaf40
hurma <karaulnihiv@gmail.com>
2021-03-01 01:37:22 +0300

    completing 2 task

---
+++ Lesson_4/Sum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Sum
+{
+    class Sum
+    {
+        static void Main(string[] args)
+        {
+            int sum = GetSum(Console.ReadLine());
+            Console.WriteLine(sum);
+        }
+
+        static int GetSum(string str)
+        {
+            int sum = 0;
+            string[] word = str.Split(' ');
+            foreach (var numbers in word)
+            {
+                sum += Convert.ToInt32(numbers) ;
+            }
+            return sum;
+        }
+
+        
+    }
+}
