f7f5b9c5236c7bdfb425bc59b4bd02bb509d09fe
hurma <karaulnihiv@gmail.com>
2021-03-01 01:34:33 +0300

    completing 1 task

---
+++ Lesson_4/FullName.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Methods
+{
+    class FullName
+    {
+        static void Main()
+        {
+            ShowUser();
+        }
+
+        static string GetFullName()
+        {
+            Console.WriteLine("Приветствую, введите имя: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Введите фамилию: ");
+            string secondName = Console.ReadLine();
+            Console.WriteLine("Введите отчество: ");
+            string patronymic = Console.ReadLine();
+
+            string result = ( secondName + " " + firstName + " " + patronymic );
+            return result;
+        }
+
+        static void ShowUser ()
+        {
+            Console.WriteLine("Введите количество пользователей:");
+            int count = Convert.ToInt32(Console.ReadLine());
+            string[] users = new string [count];
+            for (int i = 0; i < users.Length; i++)
+            {
+                users[i] = GetFullName();
+                Console.WriteLine($"Пользователь {users[i]} сохранен");
+            }
+            
+            Console.WriteLine("Ввод данных завершён. Нажмите любую клавишу...");
+            Console.Clear();
+
+            for (int i = 0; i < users.Length; i++)
+            {
+                Console.WriteLine(users[i]);
+            }
+
+        }
+
+    }
+}
