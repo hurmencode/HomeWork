5ba512211b8379a1b530d67b0063207177ac9d34
hurma <karaulnihiv@gmail.com>
2021-03-01 23:21:07 +0300

    completing an additional task

---
+++ Lesson_4/Fibanachi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace recursiv
+{
+    class Fibanachi
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Fib(n));
+        }
+        static int Fib(int number)
+        {
+            if (number == 0 || number == 1)
+            {
+                return number;
+            }
+            else
+            {
+                return (number -1) + (number - 2);
+            }
+            
+        }
+
+    }
+}
