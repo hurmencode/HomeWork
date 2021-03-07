fdb0f3346328a0439fce3ffe664f7e31da77cc54
hurma <karaulnihiv@gmail.com>
2021-03-07 18:53:27 +0300

    completing 2 task

---
+++ Lesson_5/AppendTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace AppendTime
+{
+    class AppendTime
+    {
+        static void Main(string[] args)
+        {
+            var time = DateTime.Now.ToString();
+            using var fileName = File.AppendText("startup.txt");
+            fileName.WriteLine(time);
+            Console.WriteLine($"Добавить в файл {time}");
+        }
+    }
+}
