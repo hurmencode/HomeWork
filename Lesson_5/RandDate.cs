30e22bcbc7078bacd4f86002c309699c380be5b2
hurma <karaulnihiv@gmail.com>
2021-03-07 13:19:23 +0300

    completing 1 task

---
+++ Lesson_5/RandDate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace randDate
+{
+
+    class RandDate
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "RandDate.txt";
+            string randDate = Console.ReadLine();
+            File.WriteAllText(fileName, randDate);
+        }
+    }
+}
